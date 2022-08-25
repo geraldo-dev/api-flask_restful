310e16e1bbf331347adb630841360c72a42c87b8
geraldo sousa <geraldosousadev@gmail.com>
2022-08-25 20:51:29 -0300

    function save task

--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 from tkinter import S
+from turtle import title
 from app import db
 
 class Task(db.Model):
@@ -6,3 +7,19 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return f'{self.title}'
+
+    def save_task(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def json(self):
+        return {
+            'id': self.id,
+            'title': self.title
+        }
